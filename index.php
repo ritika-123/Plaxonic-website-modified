4e08e0a168c65003c94a45096bd39447e3a928d9
ritika-123 <55281062+ritika-123@users.noreply.github.com>
2021-05-10 20:15:53 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,165 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+  <!--	<canvas class="dots">Your browser does not support canvas.</canvas>-->
+<section>
+<div class="container-fluid">
+	<div class="row">
+  
+
+  <nav class="navbar col-md-12 navbar-expand-lg navbar-light  navbar-fixed-top">
+  <a class="navbar-brand" href="index.php"><img src="images/logo.svg" alt="logo"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          SERVICES
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">TECH STACK</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">ABOUT US</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          CAREER
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">CONTACT US</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<!--navbar ends-->
+<!--carousel-->
+
+
+<div id="carousel-fade" class="carousel slide carousel-fade" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-fade" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-fade" data-slide-to="1"></li>
+    <li data-target="#carousel-fade" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+    	<div class="img-move">
+      	
+      	<img src="images/kullu2.png" class="move2 img-fluid lazy">
+      </div>
+      <div class="carousel-caption animated slideInRight">
+
+      <h2>Drive Your Business On the<br>Wheels Of Digitalization    </h2>
+        
+      </div>
+    </div>
+    <div class="carousel-item">
+    
+    	<div class="left-img">
+    		<img src="images/banner-2nd-shape.png" class="img-fluid">
+    	</div>
+      <div class="img-move">
+      	
+      	<img src="images/kullu2.png" class="move2 img-fluid lazy">
+      </div>
+      <div class="carousel-caption  animated slideInRight">
+        <h2>Keeping Clients Aligned With <br>Up-to-date Technologies  </h2>
+    </div>
+        <div class="carousel-img img-fluid animated slideInRight">
+        <img src="images/banner-2nd-img.png">
+    </div>
+        
+
+    </div>
+    <div class="carousel-item">
+    	
+    	<div class="img-move">
+    		<img src="images/banner-3rd-shape2.png" class="move1 img-fluid">
+    		<img src="images/banner-3rd-shape.png" class="move3 img-fluid">
+       	</div>
+     
+      <div class="carousel-caption animated slideInRight">
+        <h2>Channelizing Our Strengths &<br>Resources For Societal Impact</h2>
+    </div>
+
+        <div class="carousel-img img-fluid animated fadeInUp">
+        <img src="images/banner-3rd-img.png">
+      </div>
+      <div class="img-move">
+      	<img src="images/banner-3rd-shape3.png" class="move2 img-fluid">
+      	<img src="images/banner-3rd-shape4.png" class="move4 img-fluid">
+      </div>
+    
+</div>
+     <div class="carousel-item">
+     	<div class="img-move">
+     		<img src="images/banner-4th-left-corner-shape.png" style="width:5%;" class="img-fluid move2 lazy">
+     		
+     	</div>
+    
+      <div class="carousel-caption animated slideInRight">
+        <h2>Channelizing Our Strengths &<br>Resources For Societal Impact</h2>
+    </div>
+        <div class="carousel-img img-fluid animated slideInUp">
+       <img src="images/banner-4th-image.webp">
+   </div>
+     <div class="img-move">
+     		
+     		<img src="images/banner-4th-smiley-1.png" style="padding-top: 0px;" class="img-fluid move3">
+     		<img src="images/banner-4th-smiley2.png" style="padding-left: 0px;" class="img-fluid move1" >
+     	</div>
+    
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carousel-fade" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"><i class="flaticon-back"></i></span>
+   </a>
+  <a class="carousel-control-next" href="#carousel-fade" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"><i class="flaticon-next"></i></span>
+  
+  </a>
+</div>
+</div>
+<!--carousel ends-->
+<div class="after-carousel">
+	<img src="images/banner-strip.svg" class="img-fluid">
+</div>
+</div>
+</div>
+</section>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="js/script.js"></script>
+  </body>
+</html>
